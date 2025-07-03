df1f6fe51357a97e2de4afede2ffd689b93d2689
justmore5mins <justmore5mins@gmail.com>
2025-07-03 16:05:26 +0800

    add

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-
 const ImaageSlide = new Siema({
-    selector: "#SlideVideos",
-    duration: 200,
-    perPage: 1,
-  })
+  selector: "#SlideVideos",
+  duration: 100,
+  perPage: 1,
+});
+
+function checkAndRedirect() {
+  if (window.innerWidth <= 768) {
+    location.href = "/mobile";
+  } else if (location.href == location.hostname+"/mobile") {
+    location.href = "/";
+  }
+}
 
 window.onload = function () {
+  checkAndRedirect(); // 一開始先判斷
+
   // 打字機效果
   new Typed("#BigTitleLooping", {
     strings: ["FRC8569", "中和高中FRC"],
@@ -16,15 +25,21 @@ window.onload = function () {
     showCursor: false
   });
 
+  // 平滑滾動效果
   new SmoothScroll('a[href*="#"]', {
     speed: 2000
   });
 };
 
-function goNext(){
- ImaageSlide.next()
+// ⭐ 動態偵測視窗變化
+window.onresize = function () {
+  checkAndRedirect(); // 每次改變寬度就檢查
+};
+
+function goNext() {
+  ImaageSlide.next();
 }
 
-function goBack(){
-  ImaageSlide.prev()
+function goBack() {
+  ImaageSlide.prev();
 }
